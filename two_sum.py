4f97dc586556fbc5c539b7347d91a368bb2b83df
Eddy <vaismanedward@gmail.com>
2020-08-16 15:41:39 -0400

    Added two_sum problem

--- two_sum.py
+++ two_sum.py
@@ -4,6 +4,12 @@ def find_two_sum(numbers, target_sum):
     :param target_sum: (int) The required target sum.
     :returns: (a tuple of 2 ints) The indices of the two elements whose sum is equal to target_sum
     """
+
+    numberSet=set(numbers)
+    for i in numbers:
+        j=target_sum-i
+        if j in numberSet:
+            return tuple([numbers.index(i),numbers.index(j)])
     return None
 
 if __name__ == "__main__":
